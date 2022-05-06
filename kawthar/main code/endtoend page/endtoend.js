e16d1c723281c3490189fd097bc50db7cd21e11c
KawtharSafa <101676724+KawtharSafa@users.noreply.github.com>
2022-05-06 10:42:13 +0300

    kawtahr full code

---
+++ kawthar/main code/endtoend page/endtoend.js
@@ -0,0 +1,24 @@
+AOS.init();
+
+AOS.init({
+    startEvent: 'click'
+  });
+
+  // Grap elements
+const selectElement = selector => {
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw new Error(`Something went, make sure that $(selector) exists or is typed correctly.`);
+};
+
+
+// Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+
+const toggleMenu = () => {
+  const mobileMenu = selectElement('#menu');
+  mobileMenu.classList.toggle('activated');
+  menuToggleIcon.classList.toggle('activated');
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu);
